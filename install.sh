662724a4f6cf8227a8f2e9e47cca840269c1d512
James Campbell <james.campbell@tanti.org.uk>
2017-06-26 15:26:36 +0100

    create working bash profile

--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@ rm -rf ~/dotfiles
 
 git clone https://github.com/theref/dotfiles.git --recursive ~/dotfiles
 
-rm -f ~/.zshrc
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-
 rm -f ~/.gitconfig
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 
-rm -f ~/.tmuxinator
-ln -s ~/dotfiles/tmuxinator/tmuxinator.zsh ~/.tmuxinator
-
+rm -f ~/.bash_profile
+ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
