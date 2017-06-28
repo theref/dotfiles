381709c60a45d6e89f3f484a69ab0e436f740ae5
James Campbell <james.campbell@tanti.org.uk>
2017-06-28 11:51:58 +0100

    Adds xonsh settings

--- install.sh
+++ install.sh
@@ -2,8 +2,24 @@ rm -rf ~/dotfiles
 
 git clone https://github.com/theref/dotfiles.git --recursive ~/dotfiles
 
-rm -f ~/.gitconfig
+rm ~/.gitconfig
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 
-rm -f ~/.bash_profile
+rm ~/.bash_profile
 ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
+
+rm ~/.xonshshrc
+ln -s ~/dotfiles/xonsh/xonshrc.py ~/.xonshrc
+
+rm ~/.local_settings.xsh
+cp ~/dotfiles/xonsh/local_settings.xsh.example ~/local_settings.xsh
+
+# for mac
+# install homebrew
+# brew tap caskroom/versions
+# brew cask install sublime-text
+# add sublime text to path
+
+# for linux
+# 
+# need to log out, then log in to see all changes
