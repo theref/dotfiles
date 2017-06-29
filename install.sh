291dc3508a6d58f3f0da41ab627c67911c336e5c
James Campbell <james.campbell@tanti.org.uk>
2017-06-29 12:18:54 +0100

    Add git commit message syntax to dotfiles

--- install.sh
+++ install.sh
@@ -17,5 +17,7 @@ cp ~/dotfiles/xonsh/local_settings.xsh.example ~/local_settings.xsh
 
 rm '~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings'
 rm '~/Library/Application Support/Sublime Text 3/Packages/User/Python.sublime-settings'
+rm '~/Library/Application Support/Sublime Text 3/Packages/User/commit-message.sublime-settings'
 ln -s ~/dotfiles/sublime/Preferences.sublime-settings '~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings'
 ln -s ~/dotfiles/sublime/Python.sublime-settings '~/Library/Application Support/Sublime Text 3/Packages/User/Python.sublime-settings'
+ln -s ~/dotfiles/sublime/commit-message.sublime-settings '~/Library/Application Support/Sublime Text 3/Packages/User/commit-message.sublime-settings'
